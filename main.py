ea5e706bce7636693d935bb4eb078605f69007ba
LAPTOP-LF4MNVJD\spatt <spattanaik74@gmail.com>
2022-05-16 08:44:07 +0530

    initial push

---
+++ main.py
@@ -0,0 +1,69 @@
+import tkinter
+from tkinter import *
+from tkinter import ttk
+from tkinter.scrolledtext import ScrolledText
+import os
+import time
+
+
+def openFolder():
+    path= 'C:'
+    command = 'explorer.exe ' + path
+    os.system(command)
+
+
+window=Tk()
+Framebig = Frame(window)
+p1 = PhotoImage(file='./logo/email.png')
+window.iconphoto(False, p1)
+
+window.title('E.Xtraction')
+window.geometry("500x200")
+window.minsize(500, 200)
+window.maxsize(500, 200)
+
+
+
+pb = ttk.Progressbar(window, orient='horizontal', length='480')
+
+pb.grid(column=0, row=0, columnspan=2, padx=10, pady=20)
+
+text = ScrolledText(window, width=58, height=6.3)
+text.configure(state='disabled')
+text.place(x= 10, y=80)
+
+def startcommand():
+    for i in range(1, 101):
+        text.configure(state='normal')
+        text.insert(END, f'website {i} .......completed' + '\n')
+        text.configure(state='disable')
+
+
+start_button = ttk.Button(window, text="start", command=startcommand)
+stop_button = ttk.Button(window, text="stop")
+pause_button = ttk.Button(window, text="pause")
+upload_button = ttk.Button(window, text="upload", command= openFolder)
+export_button = ttk.Button(window, text="export")
+
+start_button.place(x=10, y= 50)
+stop_button.place(x= 90, y=50)
+pause_button.place(x=170, y=50)
+upload_button.place(x=335, y=50)
+export_button.place(x=415, y=50)
+
+# text = ScrolledText(window, width=58, height=6.3)
+# text.configure(state='disabled')
+# text.place(x= 10, y=80)
+
+# for i in range(1, 100):
+#     text.insert(END, f"website {i} ........completed " + '\n')
+
+
+
+# scroll = Scrollbar(window, orient=VERTICAL, command=text.yview)
+# text.config(yscrollcommand=scroll.set)
+# scroll.grid(column=1, row=0, sticky='NS')
+
+
+
+window.mainloop()
